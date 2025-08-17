6a9ddeb6b63a0e0bb5e0643387fa857777e0b486
gitxandert <alexandertoth96@yahoo.com>
2025-08-17 17:02:55 -0400

    add 'learn' route to ChatRoutes

--- backend/app/api_helpers/helper_classes.py
+++ backend/app/api_helpers/helper_classes.py
@@ -1,11 +1,11 @@
 from pydantic import BaseModel
 import llm_operations.course_building.course_builder as course_builder
-# import llm_operations.course_learning.course_teacher as course_teacher
+import llm_operations.course_teaching.course_teacher as course_teacher
 
 class ChatRoutes:
     functions = {
-        "build": course_builder.build_course
-        # "learn": course_teacher.iterate_lesson
+        "build": course_builder.build_course,
+        "learn": course_teacher.iterate_lesson
     }
 
 class ChatMsg(BaseModel):
